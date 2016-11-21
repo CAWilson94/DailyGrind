8f80be3a78eca71df2e6e55f466bf8e5d52ac42c
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-21 18:18:27 +0000

    toast added for coffee button testing

--- app/src/main/java/com/example/charl/dailygrind/MainActivity.java
+++ app/src/main/java/com/example/charl/dailygrind/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.charl.dailygrind;
 
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button_saved_coffee);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You shall not have coffee!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 /*
 Adding image view for the coffee logo
