2e71829d23664e94a452a046655ac01044071dcb
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-23 13:14:24 +0000

    list view for coffee selection

--- app/src/main/java/com/example/charl/dailygrind/SavedCoffeeActivity.java
+++ app/src/main/java/com/example/charl/dailygrind/SavedCoffeeActivity.java
@@ -2,14 +2,37 @@ package com.example.charl.dailygrind;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by charl on 22/11/2016.
  */
-public class SavedCoffeeActivity extends Activity{
+public class SavedCoffeeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.saved_coffee);
+        Toast.makeText(SavedCoffeeActivity.this, "boooop", Toast.LENGTH_SHORT).show();
+
+        String[] stuff = {"Americano", "Cappucino", "Flat White", "Fatty Latte"};
+
+        final ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stuff);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String stuffSelected = "You selected " + String.valueOf(parent.getItemAtPosition(position));
+                Toast.makeText(SavedCoffeeActivity.this, stuffSelected, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
