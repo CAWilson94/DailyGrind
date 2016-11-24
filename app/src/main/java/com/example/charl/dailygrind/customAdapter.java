27010bbccdcc74ef997ead8fb44d2883eb36af5e
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-24 16:54:10 +0000

    working layout for saved lists

--- app/src/main/java/com/example/charl/dailygrind/customAdapter.java
+++ app/src/main/java/com/example/charl/dailygrind/customAdapter.java
@@ -23,8 +23,11 @@ public class customAdapter extends ArrayAdapter<String> {
         LayoutInflater layoutInflater = LayoutInflater.from(getContext());
         View inflateView = layoutInflater.inflate(R.layout.row_layout_2, parent, false);
         String AmericanoStarbucks = getItem(position);
-        TextView theTextView = (TextView) inflateView.findViewById(R.id.text_view_row_1);
+        TextView theTextView = (TextView) inflateView.findViewById(R.id.text_view_row_2);
         theTextView.setText(AmericanoStarbucks);
+
+        TextView theTextView2 = (TextView) inflateView.findViewById(R.id.text_view_row_3);
+        theTextView2.setText(AmericanoStarbucks);
         ImageView theImageView = (ImageView) inflateView.findViewById(R.id.image_view_1);
         //theImageView.setImageResource(R.drawable.coffeeimage); - if you want to change image
         return inflateView;
