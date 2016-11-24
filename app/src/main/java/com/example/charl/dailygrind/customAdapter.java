57172ae57d14fe634aeaa60f93390413ae2af5ed
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-24 16:13:19 +0000

    layout passed to custom adapter

--- app/src/main/java/com/example/charl/dailygrind/customAdapter.java
+++ app/src/main/java/com/example/charl/dailygrind/customAdapter.java
@@ -7,7 +7,7 @@ import android.widget.ArrayAdapter;
  * Created by charl on 24/11/2016.
  */
 public class customAdapter extends ArrayAdapter<String> {
-    public customAdapter(Context context, int resource) {
-        super(context, resource);
+    public customAdapter(Context context, String[] values) {
+        super(context, R.layout.row_layout_2, values);
     }
 }
