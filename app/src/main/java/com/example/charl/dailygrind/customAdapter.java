991119f381e32773a03efa4d6939beabe38d0261
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-24 16:11:29 +0000

    constructor for extended adapter class

--- app/src/main/java/com/example/charl/dailygrind/customAdapter.java
+++ app/src/main/java/com/example/charl/dailygrind/customAdapter.java
@@ -1,7 +1,13 @@
 package com.example.charl.dailygrind;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
 /**
  * Created by charl on 24/11/2016.
  */
-public class customAdapter {
+public class customAdapter extends ArrayAdapter<String> {
+    public customAdapter(Context context, int resource) {
+        super(context, resource);
+    }
 }
