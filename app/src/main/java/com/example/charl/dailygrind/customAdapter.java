1cee50bc432463463da2aee8aff1bcb2861000c3
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-24 16:15:35 +0000

    overriding getView

--- app/src/main/java/com/example/charl/dailygrind/customAdapter.java
+++ app/src/main/java/com/example/charl/dailygrind/customAdapter.java
@@ -1,13 +1,21 @@
 package com.example.charl.dailygrind;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 /**
  * Created by charl on 24/11/2016.
  */
+
 public class customAdapter extends ArrayAdapter<String> {
     public customAdapter(Context context, String[] values) {
         super(context, R.layout.row_layout_2, values);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return super.getView(position, convertView, parent);
     }
 }
